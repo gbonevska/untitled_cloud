45f5b29bfb1c22912956e5b92824bf5c9cc3fc7b
gbonevska <gbonevska@gmail.com>
2016-01-30 18:20:45 +0200

    add phpMyAdmin to GAE
    create 3 new files app.yaml, php.ini and config.inc.php

---
+++ phpMyAdmin-4.5.4.1-english/config.inc.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This is needed for cookie based authentication to encrypt password in
+ * cookie
+ * http://www.question-defense.com/tools/phpmyadmin-blowfish-secret-generator
+ */
+$cfg['blowfish_secret'] = 'Z$Yk5#*@)T7Hi)6B}Q~dzslyx7XdfyNv6)S9'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+
+/*
+ * Servers configuration
+ */
+$i = 0;
+
+// Change this to use the project and instance that you've created.
+$host = '/cloudsql/mylibrary-1205:biblio';
+$type = 'socket';
+
+/*
+* First server
+*/
+$i++;
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
+$cfg['Servers'][$i]['socket'] = $host;
+$cfg['Servers'][$i]['connect_type'] = $type;
+$cfg['Servers'][$i]['compress'] = false;
+/* Select mysql if your server does not have mysqli */
+$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+/*
+ * End of servers configuration
+ */
+
+/*
+ * Directories for saving/loading files from server
+ */
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
+
+/*
+* Other settings
+*/
+$cfg['PmaNoRelation_DisableWarning'] = true;
+$cfg['ExecTimeLimit'] = 60;
+$cfg['CheckConfigurationPermissions'] = false;
+?>
